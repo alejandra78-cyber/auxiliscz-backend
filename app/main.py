9b173aa89c84920c0c3ec51c837574a195703f90
Alejandra <alejandraguasase7@gmail.com>
2026-04-17 21:03:25 -0400

    Avance

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://auxiliscz-web-speu.onrender.com",
     ],
     allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
