9648bf8e39a76c0c506e9c9eb8e29a0f50132988
Alejandra <alejandraguasase7@gmail.com>
2026-04-19 18:58:48 -0400

    vercel

--- app/main.py
+++ app/main.py
@@ -96,6 +96,7 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "https://auxiliscz-web-speu.onrender.com",
+        "https://auxiliscz-web.vercel.app",
     ],
     allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
